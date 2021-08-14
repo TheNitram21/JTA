b8123770f89a8c734109bbe97d63cd92fa72d3e3
NitramMann21 <martin@arnomann.de>
2021-08-14 13:53:55 +0200

    Remove Chat#connect(String, boolean)

--- src/main/java/de/arnomann/martin/jta/api/entities/Chat.java
+++ src/main/java/de/arnomann/martin/jta/api/entities/Chat.java
@@ -11,17 +11,6 @@ import java.io.IOException;
 public interface Chat {
 
     /**
-     * Connects to the chat.
-     * @param oauth The OAuth token, you can get one at <a href="https://twitchapps.com/tmi/">https://twitchapps.com/tmi/</a>
-     * @param console Whether there should be console output or not. Should be deactivated in big chats.
-     * @throws IrcException If the connection failed.
-     * @throws IOException If the bot is already connected.
-     * @deprecated Instead, use {@linkplain Chat#connect(boolean)} after setting the chat oauth token {@linkplain JTABot#setChatOAuthToken(String)}.
-     */
-    @Deprecated(since = "v2.1.0_22", forRemoval = true)
-    void connect(String oauth, boolean console) throws IOException, IrcException;
-
-    /**
      * Connects to the chat using the default OAuth token (set using {@link JTABot#setChatOAuthToken(String)}).
      * @param console Whether there should be console output or not. Should be deactivated in big chats.
      * @throws IrcException If the connection failed.
--- src/main/java/de/arnomann/martin/jta/internal/entities/ChatImpl.java
+++ src/main/java/de/arnomann/martin/jta/internal/entities/ChatImpl.java
@@ -24,15 +24,6 @@ public class ChatImpl implements Chat {
     }
 
     @Override
-    public void connect(String oauth, boolean console) throws IOException, IrcException {
-        msgbot = new MessageSenderBot("jtabot", this.bot, "#" + channel.getUser().getName());
-        msgbot.setVerbose(console);
-        msgbot.connect("irc.twitch.tv", 6667, oauth);
-        msgbot.joinChannel("#" + EntityUtils.userNameToId(channel.getUser()));
-        isConnected = true;
-    }
-
-    @Override
     public void connect(boolean console) throws IOException, IrcException {
         msgbot = new MessageSenderBot("jtabot", this.bot, "#" + channel.getUser().getName());
         msgbot.setVerbose(console);
